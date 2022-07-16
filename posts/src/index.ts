ef69a6d76dc6a4a8adf1938982bafcdc68e203b5
Marcos <marcosreisdossantos01@gmail.com>
2022-07-16 01:04:59 -0300

    feat: implement create post in memory

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,6 +1,27 @@
 import express from 'express'
-
+import {json} from 'body-parser'
+import {randomUUID} from 'crypto'
 const app = express()
+
+type PostType = { id:string, title:string}
+const posts:PostType[]=[]
+
+app.use(json())
+
+app.post("/api/posts",(req,res)=>{
+    const { title } = req.body
+    const post = {
+        id:randomUUID(),
+        title
+    }
+
+    posts.push(post)
+
+    console.log(posts)
+    
+    res.send('')
+})
+
 const port = process.env.SERVER_PORT
 app.listen(port,() =>{
     console.log(`Server started on port ${port}!`)
