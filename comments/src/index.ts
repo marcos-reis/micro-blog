a482048fc8576657cff877d09dc56c724aa62d10
Marcos <marcosreisdossantos01@gmail.com>
2022-07-16 17:04:44 -0300

    refactor: adjust in flow from posts by comments

--- comments/src/index.ts
+++ comments/src/index.ts
@@ -1,25 +1,37 @@
 import express from 'express'
-import  {json} from 'body-parser'
+import {json} from 'body-parser'
 import {randomUUID} from 'crypto'
+import axios from 'axios'
+
+
 const app = express()
 
-type CommentType = { id:string, content:string, postId:string}
-const comments:CommentType[]=[]
+type CommentType = {
+    id:string
+    content:string
+    postId:string
+}
+
+const comments:CommentType[] = []
 
 app.use(json())
 
-app.get("/api/comments",(req, res)=>{
-    const { content,postId } = req.body
-    const comment:CommentType = {
-        id:randomUUID(),
-        content,
-        postId
-    }
+const eventBusServiceUrl = process.env.EVENT_BUS_SERVICE_URL || ""
 
-    comments.push(comment)
+app.post("/posts/:id/comments",(req, res)=>{
+    const { content } = req.body
+    const postId = req.params.id
+    const id = randomUUID()
 
-    console.log(comments)
-    
+    comments.push({ id, content, postId })
+
+    axios.post(`${eventBusServiceUrl}/events`, {
+        type:"CommentCreated",
+        data:{ id, content, postId }
+    })
+    .then(_=>console.log("Event CommentCreated sent successfully"))
+    .catch(_=>console.log("Error sending event to EventBus"))    
+
     res.send('')
 })
 
--- query/src/index.ts
+++ query/src/index.ts
@@ -1,17 +1,32 @@
 import express from 'express'
 import {json} from 'body-parser'
 const app = express()
-type CommentType = { id:string, content:string}
 
+type CommentType = { id:string, content:string}
 type PostType = { id:string, title:string, comments:CommentType[]}
-const postsWithComments:PostType[]=[{
-    id:"1", title:"test",comments:[{id:"1",content:"teste 2"}]
-}]
+
+const posts:{[key:string]:PostType}={}
 
 app.use(json())
 
+app.post("/events",(req, res) => {
+    const { type, data } = req.body
+
+    if(type === "PostCreated"){
+        const {id, title} = data
+        posts[id]={ id, title, comments:[] }
+    }
+
+    if(type === "CommentCreated"){
+        const {id, content, postId} = data
+        posts[postId].comments.push({id, content})        
+    }
+    
+    res.send('')
+})
+
 app.get("/posts",(req,res) => {
-    res.send(postsWithComments)
+    res.send(posts)
 })
 
 const port = process.env.SERVER_PORT
